fe96129e9bd7e4396b71a351b0593ac84492bac9
Andrew McDonald <e_dorion@hotmail.com>
2013-06-01 22:06:25 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineSpree::Application.config.secret_token = 'c30baa5f7b76a3cbccb180e64a5650ee8d3bb70ac7786824d4ec9f24bac812335ed584af90a3fb44d8006c4c660ea8d7e643437c62cf6a0df6718d4c2d786c64'
